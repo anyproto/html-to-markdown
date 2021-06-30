0180a017555333e8da60d235bc3e448d2085a2e0
Johannes Kaufmann <johannes@joina.de>
2021-06-30 22:05:06 +0200

    fix heading inside link

--- commonmark.go
+++ commonmark.go
@@ -115,6 +115,7 @@ var commonmark = []Rule{
 			content = strings.Replace(content, "\n", " ", -1)
 			content = strings.Replace(content, "\r", " ", -1)
 			content = strings.Replace(content, `#`, `\#`, -1)
+			content = strings.TrimSpace(content)
 
 			insideLink := selec.ParentsFiltered("a").Length() > 0
 			if insideLink {
@@ -140,7 +141,6 @@ var commonmark = []Rule{
 			}
 
 			prefix := strings.Repeat("#", level)
-			content = strings.TrimSpace(content)
 			text := "\n\n" + prefix + " " + content + "\n\n"
 			return &text
 		},
